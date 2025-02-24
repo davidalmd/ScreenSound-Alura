913b104e52476c46210f49d57788dbce598c7b84
David Almeida <david.almeida@grupomoura.com>
2025-02-24 17:23:11 -0300

    feature: modificando estrutura do projeto para utilizar bibliotecas de classes

--- ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
+++ ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace ScreenSound.Banco
 {
-    internal class ScreenSoundContext: DbContext
+    public class ScreenSoundContext: DbContext
     {
         private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
         public DbSet<Artista> Artistas { get; set; }
