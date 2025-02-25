3440ef71fbf5ef65ccd3e5c21949c7d360bc30cf
David Almeida <david.almeida@grupomoura.com>
2025-02-25 17:58:29 -0300

    feature: adicionando primeiros metodos HTTP na API

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -1,6 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using ScreenSound.Banco;
+using ScreenSound.Modelos;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "API do projeto ScreenSound está rondando!");
+
+app.MapGet("/Artistas", () =>
+{
+    var DAL = new DAL<Artista>(new ScreenSoundContext());
+    return Results.Ok(DAL.Listar());
+});
+
+app.MapGet("/Artistas/{nome}", (string nome) =>
+{
+    var DAL = new DAL<Artista>(new ScreenSoundContext());
+    var artista = DAL.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
+
+    if (artista is null) return Results.NotFound();
+
+    var artistaLimpo = new Artista(artista.Nome, artista.Bio)
+    {
+        Id = artista.Id,
+    };
+
+    return Results.Ok(artistaLimpo);
+});
+
+app.MapPost("/Artistas", ([FromBody] Artista artista) =>
+{
+    var DAL = new DAL<Artista>(new ScreenSoundContext());
+    DAL.Adicionar(artista);
+    return Results.Ok();
+});
 
 app.Run();
